91fa5d9a4e70984c5f28cc681129191023c29430
myroslav <myrkozar@gmail.com>
2022-05-08 09:40:28 +0300

    с

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './sass/main.scss';
-console.log();
+console.log('hallo');
+
+const params = new URLSearchParams({
+    _limit: 2,
+    // Change the group number here
+    _page: 2
+  });
+
+fetch(`https://restcountries.com/v3.1/name?${params}`)
