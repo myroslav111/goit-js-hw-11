29d62875b3de0e8f161e4c00cb36d7baae79673b
myroslav <myrkozar@gmail.com>
2022-05-12 21:34:30 +0300

    w

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -9,18 +9,18 @@ const orientation = 'horizontal';
 const safesearch = true;
 const per_page = 40;
 
-let page = 12;
+let page = 1;
 let inputUser = '';
 
 async function fetchData(){
   if(inputUser !== refs.input.value){
     clearGallery();
-    page = 12;
+    page = 1;
   };
   inputUser = refs.input.value;
   const url = `${BASE_URL}?key=${KEY}&q=${inputUser}&image_type=${photo}&orientation=${orientation}&safesearch=${safesearch}&per_page=${per_page}&page=${page}`;
   const response = await axios.get(url);
-  if(page === 12 && response.data.totalHits !== 0){
+  if(page === 1 && response.data.totalHits !== 0){
     success(response.data.totalHits);
   };
   page += 1;
